a8d77af14df8b3748d718955c7c9f6a4b9667b89
Bruno Ferreira <108953137+brunoflma@users.noreply.github.com>
2022-11-18 23:32:52 -0300

    Arquivos iniciais

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Criando as imagens. Aguarde..."
+
+docker build -t brunoferreiram/k8sapp-backend:1.0 backend/.
+docker build -t brunoferreiram/k8sapp-database:1.0 database/.
+
+echo "Realizando login no docker. Aguarde..."
+
+docker login
+
+echo "Realizando push das imagens. Aguarde..."
+
+docker push brunoferreiram/k8sapp-backend:1.0
+docker push brunoferreiram/k8sapp-database:1.0
+
+echo "Criando os serviços. Aguarde..."
+
+kubectl apply -f ./services.yml
+
+echo "Concluindo a implantação. Aguarde..."
+
+kubectl apply -f ./deployment.yml
